ba817f4efad9f756fe187c9ec97a752a2c7b2595
Gongqi Huang <46938348+cherrypiejam@users.noreply.github.com>
2024-03-30 00:21:53 -0400

    Replace image with Linux kernel 5.10

--- profile.py
+++ profile.py
@@ -16,7 +16,7 @@ import geni.rspec.pg as rspec
 
 BASE_IP = "10.10.1"
 BANDWIDTH = 10000000
-IMAGE = 'urn:publicid:IDN+wisc.cloudlab.us+image+praxis-PG0:openwhisk-v3-5.10.130'
+IMAGE = 'urn:publicid:IDN+wisc.cloudlab.us+image+praxis-PG0:openwhisk-v3-5.10'
 
 # Set up parameters
 pc = portal.Context()
